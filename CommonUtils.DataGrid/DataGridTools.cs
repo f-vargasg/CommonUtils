74d7321f99de2b6e037000d3cbce7da402df5db5
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2022-11-14 10:58:26 -0600

    Mejoras en condicion para sql (Escritorio)

---
+++ CommonUtils.DataGrid/DataGridTools.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommonUtils.DataGrid
+{
+    public class DataGridTools
+    {
+
+        public DataGridView DgrdView { get; set; }
+
+        public DataGridTools() { }
+
+        public DataGridTools(DataGridView pDgrdV)
+        {
+            DgrdView = pDgrdV;
+        }
+
+        public void HideColumns(string colToHide)
+        {
+            string[] strings = colToHide.Split(',');
+            foreach (var colStr in strings)
+            {
+                int ind = Convert.ToInt32(colStr);
+                this.DgrdView.Columns[ind].Visible = false;
+            }
+        }
+        public void FillDataGrid<T>(List<T> list)
+        {
+            var bindingList = new BindingList<T>(list);
+            var source = new BindingSource(bindingList, null);
+            this.DgrdView.DataSource = source;
+        }
+
+        public void FillDataGrid<T>(List<T> list,
+                                    string plstColsToHide)
+        {
+            var bindingList = new BindingList<T>(list);
+            var source = new BindingSource(bindingList, null);
+            this.DgrdView.DataSource = source;
+            HideColumns(plstColsToHide);
+        }
+    }
+}
