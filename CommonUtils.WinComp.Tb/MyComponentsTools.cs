d934a9aa8b860eef086813403e76eaac4998851a
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2023-02-01 14:42:15 -0600

    Ajustes en dll para rutinas de componentes winforms

---
+++ CommonUtils.WinComp.Tb/MyComponentsTools.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommonUtils.WinCompTb
+{
+    public class MyComponentsTools
+    {
+        public static void SetAccessToUserControls(IEnumerable ctrlCollection, bool pDispCtrls)
+        {
+            try
+            {
+                foreach (Control control in ctrlCollection)
+                {
+                    if (control is TextBox)
+                    {
+                        control.Enabled = pDispCtrls;
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
